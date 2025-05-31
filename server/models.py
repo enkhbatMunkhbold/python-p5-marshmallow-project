a2bad37462bf9843b078119990c888e3298ca7d9
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2025-05-31 13:57:44 -0700

    Add User and Flight models

--- server/models.py
+++ server/models.py
@@ -1,5 +1,38 @@
+from datetime import datetime
 from sqlalchemy.ext.hybrid import hybrid_property
+from config import db, bcrypt, ma
 
-from config import db, bcrypt
 
-# Models go here!
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    _password_hash = db.Column(db.String, nullable=False)
+
+    flights = db.relationship('Flight', secondary='bookings', viewonly=True)
+    airlines = db.relationship('Airline', secondary='bookings', viewonly=True)
+
+    @hybrid_property
+    def password_hash(self):
+        return self._password_hash
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password.encode('utf-8'))
+
+class Flight(db.Model):
+    __tablename__ = 'flights'
+
+    id = db.Column(db.Integer, primary_key=True)
+    origin = db.Column(db.String(100), nullable=False)
+    destination = db.Column(db.String(100), nullable=False)
+    departure_time = db.Column(db.DateTime, nullable=False)
+    arrival_time = db.Column(db.DateTime, nullable=False)
+
+    bookings = db.relationship('Booking', backref='flight', lazy=True)
